3762f1f7fe8f0db6a4e51794c556f16c159ac7a9
NosiphiwoB <biyelanosiphiwo@gmail.com>
2023-02-06 15:55:21 +0200

    back end

--- back-end/index.js
+++ back-end/index.js
@@ -1,28 +1,25 @@
 const express = require('express')
-require('dotenv').config()
-const app = express()
-const cors = require('cors')
-const port = process.env.PORT
-const URI = process.env.DATA_BASE
+const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-const { userRoute } = require('./routes/user')
-const { blogRoute } = require('./routes/blog')
+const app = express()
+const {userRoute} = require('./routes/user');
+const {blogRoute} = require('./routes/blogs')
 
 
+app.use(bodyParser.json())
 
-app.use(express.json())
-app.use(express.urlencoded({extended: "false"}))
-app.use(cors())
-mongoose.connect(
-    URI,{
-      useNewUrlParser: true, 
-      useUnifiedTopology: true,
-    }
-  ).then(res => console.log('("Connected")')).catch(err => console.log(err))
-  console.log("URI",URI)
-  mongoose.set('strictQuery', true)
-userRoute(app)
+
+const  url = "mongodb+srv://NosiphiwoB:biyela0305@cluster0.lr5epya.mongodb.net/Reg-Log-in_Form?retryWrites=true&w=majority"
+mongoose.connect(url, { useUnifiedTopology: true, useNewUrlParser: true });
+const connection = mongoose.connection;
+connection.once("open", function() {
+  console.log("Connected");
+});
+
+
 blogRoute(app)
+userRoute(app)
 
-
-app.listen(port, () => console.log(`App listening on port ${port}`))
+app.listen(4008 , () => {
+   console.log("port running on port 4008")
+})
